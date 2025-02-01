23b57c0473bc0bad55d968d7388916bf508e5058
bjones718 <bjones718@mcad.edu>
2025-02-01 12:15:46 -0600

    Add files via upload

---
+++ libraries/week2/favoritemovies/main.js
@@ -0,0 +1,8 @@
+
+const movies = ["Shawshank Redemption", "English Patient", "Star Wars", "The Girl With the Dragon Tattoo", "Animal House"];
+// prompt
+let input = prompt("what is your favorite movie?");
+// input
+movies.push(input);
+// log
+console.log(movies.toString())
